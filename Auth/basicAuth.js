691282d7b3e12872d1107f6dc484f9f86a752781
ASPIRE-INFOTECH\hObeidat <hamzehob94@gmail.com>
2021-08-18 09:26:09 +0300

    modified file

--- Auth/basicAuth.js
+++ Auth/basicAuth.js
@@ -6,21 +6,13 @@ const app = express();
 app.use(express.json())
 
 function authUser(req, res, next) {
-  // const user =  User.findOne({ where: { email: req.body.email } });
-    // if (req.user === null) {
-    //   // console.log(user);
-    //   console.log('Not found!');
-    //   res.status(403)
-    //   return res.send('You need to sign in')
-    // } 
-    // console.log(user);
-
+  
     User.findOne({  where: { email: "hamzeh@gmail.com" } }).then((user) => {
       if (!user){
         console.log('Not found!');
         res.status(403)
         return res.send('You need to sign in')
-        // return res.json({message: `There is no record of the email ${email}.`} )
+        
       }
 
      })
--- server.js
+++ server.js
@@ -9,10 +9,7 @@ const app = express();
 app.use(express.json())
 
 //database
-
 const db= require('./config/database')
-// app.use(setUser)
-// app.get('/users',userRoute)
 
 
   try {
@@ -29,7 +26,7 @@ app.get('/', (req, res) => {
 })
 
 app.use('/user', require('./routes/user'))
-app.use('/auth', require('./routes/auth'))
+
 
 // admin page
 app.get('/admin',  authRole('admmin'), (req, res) => {
@@ -42,104 +39,6 @@ app.get('/exam', authUser, (req, res) => {
     res.send('exam page')
 })
 
-// function setUser(req, res, next) {
-//     const userId = req.body.userId
-//     if (userId) {
-//       req.user = users.find(user => user.id === userId)
-//     }
-//     next()
-//   }
-
-  // Option 2: Passing parameters separately (other dialects)
-
-// // create connection
-// const db = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "",
-//     database: "epicexam"
-// });
-
-// // connect to mysql
-// db.connect((err) => {
-//     if (err) {
-//         throw err
-//     }
-//     console.log("mysql connection");
-// })
-
-
-
-// //create database
-// app.get('/createdb',(req, res) => {
-//     let sql = "CREATE DATABASE epicexam";
-//     db.query(sql, (err) => {
-//         if (err) {
-//             throw err;
-//         }
-//         res.send('database created')
-//     })
-// })
-
-// // create table 
-// app.get('/createuser', (req, res) => {
-//     let sql = 'CREATE TABLE user(id int AUTO_INCREMENT, name VARCHAR(255), email VARCHAR(255), password VARCHAR(255), PRIMARY KEY(id))'
-//     db.query(sql, err => {
-//         if (err) {
-//             throw err
-//         }
-//         res.send('user table created')
-//     })
-// })
-
-// //insert user
-// app.get('/employee1', (req,res) => {
-//     let user = {name: 'Hamzeh Obeidat', email:'hamzeh@gmail.com',password: '123456'}
-//     let sql = 'INSERT INTO user SET ?'
-//     let query = db.query(sql, user, err => {
-//         if (err) {
-//             throw err
-//         }
-//         res.send('user added')
-//     })
-// })
-
-// //select users
-// app.get('/getuser', (req,res) => {
-//     let sql = 'SELECT * FROM user'
-//     let query = db.query(sql, (err, results) => {
-//         if (err) {
-//             throw err
-//         }
-//         console.log(results);
-//         res.send('user details fetched')
-//     })
-// })
-
-
-// //update user
-// app.get('/updateuser/:id',(req, res) => {
-//     let newPassword = "654321"
-//     let sql = `UPDATE user SET password = ${newPassword} WHERE id = ${req.params.id}`
-//     let query = db.query(sql, err => {
-//         if (err) {
-//             throw err
-//         }
-//         res.send('user updated')
-//     }) 
-// })
-
-
-// // delete user
-// app.get('/deleteuser/:id', (req, res)=> {
-//     let sql = `DELETE FROM user WHERE id = ${req.params.id}`
-//     let query = db.query(sql, err => {
-//         if (err) {
-//             throw err
-//         }
-//         res.send('user deleted')
-//     })
-// })
 
 
 
